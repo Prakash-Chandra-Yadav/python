da5ca3b268a6de877572258e7c903a47bedd05bf
Prakash C yadav <pcyd100@gmail.com>
2026-05-26 14:03:43 +0545

    varaible rules in python

--- Variables/hello_world.py
+++ Variables/hello_world.py
@@ -1,2 +1,5 @@
 message = "Python refreshment"
 print(message)
+
+rules_of_variables ="1.variables shold be in the lowercase\n2.variable can start with the letter and underscores but cant start with the number.\n3.avoid the python keywords while declaring the variables.\n4.variables name should be short but meaningful"
+print(rules_of_variables)
