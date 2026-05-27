94a1e2e70a853d0e17f19b52c4e4067baefeff44
Prakash C yadav <pcyd100@gmail.com>
2026-05-27 13:38:52 +0545

    studied string error types

--- Strings/Removing prefixes.py
+++ Strings/Removing prefixes.py
@@ -3,3 +3,9 @@
 abc_url ='https://abc.com'
 display_url = abc_url.removeprefix('https://')
 print(display_url)
+
+##"avoiding the apostrophe error"
+message = "one of the python's strength is its diverse community."
+print(message)
+
+##if we have used the single quotes it would have produces the error
