e6d49ad2dcb2ae5e96f41f84e4a8b768a67c120f
Prakash C yadav <pcyd100@gmail.com>
2026-05-27 13:06:55 +0545

    new exercise

---
+++ Strings/name.py
@@ -0,0 +1,6 @@
+name = "ada lovelace"
+print(name.title()) 
+
+##print the name of the variable it should be in the lower case 
+variable = input("enetr the variable and i will convert into the correct format")
+print(variable.lower())
