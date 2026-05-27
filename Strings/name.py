8fd7f7411963b87c267b8ce5039509ab089e2f4d
Prakash C yadav <pcyd100@gmail.com>
2026-05-27 13:10:51 +0545

    ex2

--- Strings/name.py
+++ Strings/name.py
@@ -1,6 +1,13 @@
 name = "ada lovelace"
 print(name.title()) 
 
-##print the name of the variable it should be in the lower case 
-variable = input("enetr the variable and i will convert into the correct format")
-print(variable.lower())
+# ##print the name of the variable it should be in the lower case 
+# variable = input("enetr the variable and i will convert into the correct format")
+# print(variable.lower())
+
+##fullname .py 
+first_name = "prakash"
+last_name = "chandra"
+full_name = f"{first_name} {last_name}"
+message = (f"hello!! {full_name.title()}")
+print(message)
