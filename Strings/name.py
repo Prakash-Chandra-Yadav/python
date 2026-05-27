ffdb67995d76d3e848f3330a7b2f2158059f2b47
Prakash C yadav <pcyd100@gmail.com>
2026-05-27 13:27:02 +0545

    ex4

--- Strings/name.py
+++ Strings/name.py
@@ -13,7 +13,23 @@ print(name.title())
 # print(message)
 
 
-##adding white spaces into the string 
-print("python")
-print("\tPython 'i have use the tab here'")
-print("some programming languages are:\nPython\nJava\nC")
+# ##adding white spaces into the string 
+# print("python")
+# print("\tPython 'i have use the tab here'")
+# print("some programming languages are:\nPython\nJava\nC
+
+# ##removing the white spaces (what if the user add the extras spaces i the crediantials)
+ ##problem
+name = "Prakash"
+username = input("please! enter your user name: ")
+if username == name: 
+    print("wellcome!!")
+else: 
+    print("sorry the crediantials is wrong!!") #user will get the error if he will put the extra spaces in his name 
+
+print("this is the solution we can strip out the spaces after or before the name")
+
+if (username.lstrip() == name  or username.rstrip() == name): 
+    print("wellcome!!")
+else: 
+    print("sorry the crediantials is wrong!!")
