2c2a5e5da22cc4bfbab2c2e585cabb61a051a82d
Prakash C yadav <pcyd100@gmail.com>
2026-05-27 13:44:51 +0545

    how does python treat the number (integer)

---
+++ Strings/numbers.py
@@ -0,0 +1,10 @@
+##how does the python sees the number in the program
+print(2+3)
+print(3-2)
+print(3/2)
+print(3**2)
+print(10**6)
+
+##by default python supports the order of operation mathematically, but if you want to change the order you can use the () parentheses 
+print(2+3*2)
+print((2+3)*2)
