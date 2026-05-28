bfeaa2e6b94878bba9da22f8803333459f4026c2
Prakash Chandra <prakashworks98@gmail.com>
2026-05-28 15:28:46 +0545

    python numbers

---
+++ Comments/Comments.py
@@ -0,0 +1,4 @@
+##comments
+##comments are written in the python to keep track of the code work flow in the lines of the code 
+
+print("'#' us used fo the commenting in a single line in the python, comment is used to add the description in the linse of the code so that the debugging and working with the other programmer would be easier")
