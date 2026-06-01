7c6f328de84174d0d911df629fb38fdedf9a29c5
Prakash Chandra <prakashworks98@gmail.com>
2026-06-01 10:42:45 +0545

    List exercise1

---
+++ List/list.py
@@ -0,0 +1,11 @@
+##how does list looks like ?
+bicycles = ["trek","cannodale","redline","specialized"]
+
+##what if you want to print the name of your first bicycle
+message = f"my first bicycle was {bicycles[0]}"
+print(message)
+
+##what if someone asks you what was your last bicycle 
+next_message = f"my last bicycle was {bicycles[-1]}"
+print(next_message)
+
---
+++ Numbers/mathmodule.py
@@ -0,0 +1,9 @@
+import math 
+
+x = 2
+y = 2
+z = math.remainder(x,y) 
+print(z)
+
+##can we get the value of the pi
+print(math.pi)
