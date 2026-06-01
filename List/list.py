fba90e64de7a63bcd81fcf290d91cb717402e9f1
Prakash Chandra <prakashworks98@gmail.com>
2026-06-01 11:36:11 +0545

    practice questions

--- List/list.py
+++ List/list.py
@@ -49,3 +49,79 @@ motorcycles = ["hero","tvs","bajaj","yamaha","ninja"]
 print(motorcycles)
 motorcycles.remove('bajaj')
 print(motorcycles)
+
+##NOTe: remove method only removes the first occurance of the values 
+##ty it yourself 
+
+#3.4 
+invitations = ["ram","shyam","ghanshyam","ramu","bibek", "abishek"]
+print(f"{invitations[0]} is invited")
+print(f"{invitations[1]} is invited")
+print(f"{invitations[2]} is invited")
+print(f"{invitations[3]} is invited")
+print(f"{invitations[4]} is invited")
+print(f"{invitations[5]} is invited")
+
+
+#3.5 
+invitations = ["ram","shyam","ghanshyam","ramu","bibek", "abishek"]
+print(f"{invitations[0]} is invited")
+print(f"{invitations[1]} is invited")
+print(f"{invitations[2]} is invited")
+print(f"{invitations[3]} is invited")
+print(f"{invitations[4]} is invited")
+print(f"{invitations[5]} is invited")
+
+print(f"we have been informed that {invitations[4]} cant make it")
+
+del invitations[4]
+invitations.insert(4,'billu')
+
+print(f"{invitations[0]} is invited")
+print(f"{invitations[1]} is invited")
+print(f"{invitations[2]} is invited")
+print(f"{invitations[3]} is invited")
+print(f"{invitations[4]} is invited")
+print(f"{invitations[5]} is invited")
+
+#3.6
+del invitations[4]
+invitations.insert(4,'billu')
+
+print(f"{invitations[0]} is invited")
+print(f"{invitations[1]} is invited")
+print(f"{invitations[2]} is invited")
+print(f"{invitations[3]} is invited")
+print(f"{invitations[4]} is invited")
+print(f"{invitations[5]} is invited/n")
+
+print("so we have bigger table and we have plenty of spaces")
+
+invitations.insert(0,'Prakash')
+invitations.insert(2,'Raju')
+invitations.append('Sita')
+print(invitations)
+print(f"{invitations[0]} is invited")
+print(f"{invitations[1]} is invited")
+print(f"{invitations[2]} is invited")
+print(f"{invitations[3]} is invited")
+print(f"{invitations[4]} is invited")
+print(f"{invitations[5]} is invited")
+print(f"{invitations[6]} is invited")
+print(f"{invitations[7]} is invited")
+print(f"{invitations[8]} is invited")
+
+#3.7
+print("sorry to inform but we only have space for two guests")
+while len(invitations) >2:
+    popped = invitations.pop()
+    print(f"{popped} has been removed from the list")
+   
+for name in invitations:
+    print(f"{name} is invited")
+
+print(invitations)
+
+del invitations[0]
+del invitations[0]
+print(invitations)
