6725a602c310ca4f608fc7a7570f91cea7ccf402
Prakash Chandra <prakashworks98@gmail.com>
2026-06-02 11:11:01 +0545

    Dictionaries

---
+++ Dictionaries/Dictionaries.py
@@ -0,0 +1,8 @@
+alien_0 = {'color':'green','points':5}
+print(alien_0['color']) #w have provided the key here so it will print the value 
+
+#adding new key value pair in a dictionaries 
+print(alien_0)
+alien_0['x_positions'] = 0
+alien_0['y_positions'] = 25
+print(alien_0)
