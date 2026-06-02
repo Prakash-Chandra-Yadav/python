7401f7a283d660e1799b7b5bac406580167dd623
Prakash Chandra <prakashworks98@gmail.com>
2026-06-02 11:03:06 +0545

    done exercises

--- If Statements/If_Statements.py
+++ If Statements/If_Statements.py
@@ -6,3 +6,39 @@ if "peperoni" in requested_toppings:
 if "extra cheese" in requested_toppings: 
     print("adding extra cheese")
 print("\n finished making your pizza!!")
+
+##try yourself 
+#5.3
+alien_color = "green"
+if alien_color == "green":
+    print("you have just earned 5 points")
+else: 
+    print("")
+#5.4
+alien_color = "green"
+if alien_color == "green":
+    print("you have just earned 5 points")
+else: 
+    print("you have earned 10 points")
+
+#5.5
+alien_color = "green"
+if alien_color == "green":
+    print("you have just earned 5 points")
+if alien_color == "yellow":
+    print("you have just earned 10 points")
+if alien_color == "red":
+    print("you have just earned 15 points")
+else: 
+    print("")
+
+##using if statement in a list 
+##using multiple lists 
+available_toppings = ["mushrooms","olives","green papers","pepproni","pineapple","extra cheese"]
+requested_toppings = ["mushrooms","french fries","extra cheese"]
+
+for requested_topping in requested_toppings: 
+    if requested_topping in available_toppings:
+        print(f"adding {requested_topping} in your pizza!!")
+    else: 
+        print(f"sorry! we are out of {requested_topping}")
