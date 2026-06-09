59d1ca5597ce862bc86de1b797ef6998969d6c2e
Prakash Chandra <prakashworks98@gmail.com>
2026-06-09 13:31:31 +0545

    added the exercises of the importing class

---
+++ importing classes/my_car.py
@@ -0,0 +1,4 @@
+from car import Car
+my_new_car = Car('audi','a4',2020)
+my_new_car.get_descriptive_name()
+my_new_car.read_odometer()
---
+++ importing classes/my_electric.py
@@ -0,0 +1,6 @@
+from car import ElectricCar
+my_leaf = ElectricCar('nissan','leaf',2025)
+my_leaf.get_descriptive_name()
+my_leaf.fill_gas_tank()
+my_leaf.read_odometer()
+my_leaf.battery.show_battery_capacity()
