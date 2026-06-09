636272d0c4616a40a5697183518f229effada974
Prakash Chandra <prakashworks98@gmail.com>
2026-06-09 15:35:20 +0545

    completed the standard library exercise

--- python standard library/python_library.py
+++ python standard library/python_library.py
@@ -8,3 +8,43 @@ class Dice:
             print(face)
 d1 = Dice(6)
 d1.roll_dice()
+
+
+class Lottery: 
+    def __init__(self,*combinations):
+        self.combinations = combinations
+    def create_lottery(self):
+        lottery = [] 
+        for i in range(1,5):
+            character = choice(self.combinations)
+            lottery.append(character)
+        self.see_lottry(lottery)
+        return lottery
+            
+    def see_lottry(self,lottery):
+        print("any ticked matching these 4 numbers or letter wins the prize: ")
+        for char in lottery:
+            print(char,end='')
+            
+l1 = Lottery(1,4,5,6,8,8,5,3,1,'a','b','c','d','e','f')
+winning_ticket = l1.create_lottery()
+Lottery_range = (1,4,5,6,8,8,5,3,1,'a','b','c','d','e','f')
+attempts = 0 
+while True: 
+    my_ticket = []
+    for i in range(1,5):
+        choice_number = choice(Lottery_range)
+        my_ticket.append(choice_number)
+    if my_ticket == winning_ticket:
+        print("you win the lottery")
+        print(f"-----you won the lottery, you have tried {attempts} times-----")
+        break
+    else:
+        attempts += 1
+        print(f"you lose the lottery, you have tried {attempts} times")
+
+
+    
+
+
+
