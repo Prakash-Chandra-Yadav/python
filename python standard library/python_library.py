2b5619c2bf3a01559c87b492416c86ed959537de
Prakash Chandra <prakashworks98@gmail.com>
2026-06-09 15:03:26 +0545

    added the standard library exercise

--- python standard library/python_library.py
+++ python standard library/python_library.py
@@ -1,5 +1,10 @@
 from random import randint,choice 
-print(randint(1,10))
-players = ['charles','Martina','Michael','Florance','Eli']
-first_up = choice(players)
-print(first_up)
+class Dice: 
+    def __init__(self,side):
+        self.side = side 
+    def roll_dice(self):
+        for  x in range(1,11):
+            face = randint(1,self.side)
+            print(face)
+d1 = Dice(6)
+d1.roll_dice()
