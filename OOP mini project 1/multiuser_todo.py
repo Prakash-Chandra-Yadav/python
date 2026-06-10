cd95f5233e25deaa7281ae5048c3da319ddf4ea1
Prakash Chandra <prakashworks98@gmail.com>
2026-06-10 16:03:58 +0545

    added the exercise of the reading files

---
+++ OOP mini project 1/multiuser_todo.py
@@ -0,0 +1,6 @@
+print("this is the app made for the to do list, you can create an account and add your task to the list, you can also see the task that you have added and delete the task that you have completed" \
+"" \
+"let's get started!!")
+user_info = {}
+def create_account():
+    
