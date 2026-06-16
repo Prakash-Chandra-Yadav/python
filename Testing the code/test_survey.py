fea9d37a6d27c0ac356f2c41a55ef36f12540668
Prakash Chandra <prakashworks98@gmail.com>
2026-06-16 12:53:33 +0545

    learned about the pytest fixture

--- Testing the code/test_survey.py
+++ Testing the code/test_survey.py
@@ -1,16 +1,20 @@
 #create the new test class 
+import pytest
 from survey import AnonymousSurvey
-def test_store_single_response():
+@pytest.fixture
+def language_survey():
+    '''a survey object that will be avaliavle to all the functions'''
+    question = 'what language did you first learned to speak?'
+    language_survey = AnonymousSurvey(question)
+    return language_survey
+
+def test_store_single_response(language_survey):
     '''test that the single response is stored corerctly'''
-    question = "what language did you first learned to speak?"
-    language_survey = AnonymousSurvey(question )
     language_survey.store_responses('English')
     assert 'English' in language_survey.responses
 
 ##now lets test if the class method can take more than one values 
-def test_store_three_responses():
-    question = "whats the first language that you learned?"
-    language_survey = AnonymousSurvey(question)
+def test_store_three_responses(language_survey):
     responses = ['English','German','Spanish']
     for response in responses:
         language_survey.store_responses(response)
