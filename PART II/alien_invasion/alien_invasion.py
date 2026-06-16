c0581991420ba116083341b319cba488de316006
Prakash Chandra <prakashworks98@gmail.com>
2026-06-16 15:34:25 +0545

    feat: added the ship class and new helper method _update_events and _update_screen

--- PART II/alien_invasion/alien_invasion.py
+++ PART II/alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys 
 import pygame 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     '''overall class to manage the game assets and behaviors'''
@@ -11,20 +12,32 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("ALIEN INVASION")
+        self.ship = Ship(self)
+
         #set the background color
         self.bg_color = (230,230,230)
     def run_game(self):
         '''start the main loop for the game'''
         while True: 
             #watch for the keyboard respoisne and events 
-            for event in pygame.event.get():
+            self._check_events()
+            #redraw the screen during each pass through the loop
+            self._update_screen()
+            self.clock.tick(60)
+    def _check_events(self):
+        '''responds to the keypress and mouse events'''
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            #redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            #make most recently drawn screen visible 
-            pygame.display.flip()
-            self.clock.tick(60)
+    def _update_screen(self):
+        '''updates the image on the screen and flip to the new screen'''
+         #redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #make most recently drawn screen visible 
+        pygame.display.flip()
+
+
 if __name__ == '__main__':
     #make the game instance and run the game 
     ai = AlienInvasion()
