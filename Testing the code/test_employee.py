7e939d270a1a1a13e896c43b82ae169cd25f770e
Prakash Chandra <prakashworks98@gmail.com>
2026-06-16 13:16:41 +0545

    created the test case using the pytest fixture

--- Testing the code/test_employee.py
+++ Testing the code/test_employee.py
@@ -1,13 +1,16 @@
 from employee import Employeee
 import pytest 
-
-def test_give_defualt_raise():
-    '''test if the default raise is being added in the annual salary'''
+@pytest.fixture
+def emp1():
+    '''create one objet for all the test functions'''
     emp1 = Employeee('Prakash','Chandra',10000)
+    return emp1
+
+def test_give_defualt_raise(emp1):
+    '''test if the default raise is being added in the annual salary'''
     emp1.give_raise()
     assert emp1.annual_salary == 15000
-def test_give_custom_raise():
+def test_give_custom_raise(emp1):
     '''test if the custom raise is being added in the annual salary'''
-    emp1 = Employeee('Prakash','Chandra',10000)
     emp1.give_raise(8000)
     assert emp1.annual_salary == 18000
