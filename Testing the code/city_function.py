cdcad6f2ec8d20e825d3ff45a0a8f79de033d211
Prakash Chandra <prakashworks98@gmail.com>
2026-06-16 11:32:14 +0545

    completed the initial exercises

--- Testing the code/city_function.py
+++ Testing the code/city_function.py
@@ -1,4 +1,7 @@
-def get_city_name(city,country):
+def get_city_name(city,country,population=0):
     '''return the name of the city along with the country'''
-    city_info = f'{country.title()}, {city.title()}'
+    if population:
+        city_info = f'{country.title()}, {city.title()} -population {population}'
+    else: 
+        city_info = f'{country.title()}, {city.title()}'
     return city_info
--- Testing the code/test_cities.py
+++ Testing the code/test_cities.py
@@ -3,3 +3,7 @@ def test_city_country():
     '''lets see if the function works correctly'''
     city_details = get_city_name('Kathmandu','Nepal')
     assert city_details == 'Nepal, Kathmandu'
+def test_city_popiulation():
+    '''lets check if the function accepts the population'''
+    city_details = get_city_name('kathmandu','Nepal',population=10000)
+    assert city_details == 'Nepal, Kathmandu -population 10000'
