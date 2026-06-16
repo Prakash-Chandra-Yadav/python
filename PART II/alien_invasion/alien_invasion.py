96db17ed17bcfd6088093d49d4bd15e44eebee69
Prakash Chandra <prakashworks98@gmail.com>
2026-06-16 14:12:55 +0545

    feat: created the alien invasion class and the setting class

---
+++ PART II/alien_invasion/alien_invasion.py
@@ -0,0 +1,31 @@
+import sys 
+import pygame 
+from settings import Settings
+
+class AlienInvasion:
+    '''overall class to manage the game assets and behaviors'''
+    def __init__(self):
+        '''initialize the game and create the game resources'''
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("ALIEN INVASION")
+        #set the background color
+        self.bg_color = (230,230,230)
+    def run_game(self):
+        '''start the main loop for the game'''
+        while True: 
+            #watch for the keyboard respoisne and events 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+            #make most recently drawn screen visible 
+            pygame.display.flip()
+            self.clock.tick(60)
+if __name__ == '__main__':
+    #make the game instance and run the game 
+    ai = AlienInvasion()
+    ai.run_game()
