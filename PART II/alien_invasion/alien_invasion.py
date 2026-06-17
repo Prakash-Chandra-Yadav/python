f54989d1e00dcfc92af0914065b8f0c5d1561349
Prakash Chandra <prakashworks98@gmail.com>
2026-06-17 13:51:46 +0545

    feat: update the logic of the create alien and create fleet method to create the fleet of the aliens in the horizontal and vertical as well

--- PART II/alien_invasion/alien_invasion.py
+++ PART II/alien_invasion/alien_invasion.py
@@ -85,17 +85,25 @@ class AlienInvasion:
         '''create the fleet of the aliens'''
         #make an alien 
         #create the alien and keep adding aliens until theres no room left 
-        #spacing between aliens is one alien width 
+        #spacing between aliens is one alien width and one to the height  
         alien = Alien(self)
-        alien_width = alien.rect.width 
+        alien_width,alien_height = alien.rect.size 
 
-        current_x = alien_width 
-        while current_x < (self.settings.screen_width - 2*alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2*alien_width
+        current_x,current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):  
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x,current_y)
+                current_x += 2 * alien_width
+            #finished a row, reset the x avlue and increment the y value
+            current_x = alien_width
+            current_y += 2 * alien_height
+    def _create_alien(self,x_position,y_position):
+        '''create an alien and place it in the row'''
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _update_screen(self):
         '''updates the image on the screen and flip to the new screen'''
