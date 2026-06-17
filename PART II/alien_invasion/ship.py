5f6ac2bd4ff0551c568b1ae36d24d520a643eb9c
Prakash Chandra <prakashworks98@gmail.com>
2026-06-17 11:12:12 +0545

    feat: added the logic to move the ship to both left and right and adjusted the speed of the ship by adding the speed attribute in the settings class

--- PART II/alien_invasion/ship.py
+++ PART II/alien_invasion/ship.py
@@ -1,10 +1,12 @@
 import pygame 
+import settings
 class Ship: 
     '''a class to manage the ship'''
     def __init__(self,ai_game):
         '''initialize the ship and set its starting position'''
         self.screen = ai_game.screen 
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings 
 
         ##load the ship image and get its rect 
         self.image =pygame.image.load('images/ship.bmp')
@@ -13,12 +15,22 @@ class Ship:
         ##start each new ship at the buttom center of the screen 
         self.rect.midbottom = self.screen_rect.midbottom 
 
+        #store a float for the ships exact horizontal position 
+        self.x = float(self.rect.x)
+
+
         #movement flag; start with a ship that is not moving 
         self.moving_right = False
+        self.moving_left = False
     def update(self):
         '''updates the ships position based on the movement flag'''
+        #update the x calue not the rect
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed 
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        #update the object from self.x
+        self.rect.x = self.x
     def blitme(self):
         '''draw the ship at its current location'''
         self.screen.blit(self.image,self.rect)
